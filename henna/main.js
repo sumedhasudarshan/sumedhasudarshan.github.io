474a2536478f83239b1be18d8bcc52d2dd8d48f9
sumedhasudarshan <63002766+sumedhasudarshan@users.noreply.github.com>
2020-04-01 19:38:06 +0530

    Add files via upload

---
+++ henna/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var submitButton = document.getElementById('buttonid');
+
+    submitButton.addEventListener('click', function(event) {
+
+        alert('THANK YOU FOR SUBMITTING, PAWESOME FRIEND!');
+
+})});
